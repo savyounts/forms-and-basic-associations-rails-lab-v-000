b959a3d601c2e627ba2dc14f4868e3600109ab0e
Savannah Younts <savyounts@gmail.com>
2018-09-24 17:02:40 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Arist.find_or_create_by(name: name)
+  end
+
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end
 end
