be02b393ff5a0b8255b6be315b93dc1d20b07164
Savannah Younts <savyounts@gmail.com>
2018-09-24 17:04:10 +0000

    automatically backed up by learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,11 @@
 class Note < ActiveRecord::Base
   belongs_to :song
   # add associations here
+
+  def song_ids=(ids)
+    ids.each.do |id|
+      song = Song.find(id)
+      self.songs << song 
+    end
+  end
 end
