fc68cf86e5fa9205274f8c2f20c507d57796ce53
Savannah Younts <savyounts@gmail.com>
2018-09-24 18:00:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents
     if self.notes
       self.notes.collect do |n|
-        n.content unless n.blank?
+        n.content unless n.empty?
       end
     end
   end
