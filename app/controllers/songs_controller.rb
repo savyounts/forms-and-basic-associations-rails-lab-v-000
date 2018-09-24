5cf9d010bf589668d40ce6de3497dc5d421b5549
Savannah Younts <savyounts@gmail.com>
2018-09-24 17:17:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :genre_name, :artist_name, note_ids:[])
+    params.require(:song).permit(:title, :genre_id, :artist_name, note_ids:[])
   end
 end
