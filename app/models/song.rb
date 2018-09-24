ad543b7e789ac169c09d257dab925311b68741d6
Savannah Younts <savyounts@gmail.com>
2018-09-24 17:12:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,10 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def note_ids=(ids)
+    ids.each.do |id|
+      note = Note.find_or_create_by(id: id)
+      self.notes << note
+    end
 end
