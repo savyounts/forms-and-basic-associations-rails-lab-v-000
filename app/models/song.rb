32958e96731b52923af2606d4c98d7ed842bc924
Savannah Younts <savyounts@gmail.com>
2018-09-24 17:57:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -32,6 +32,9 @@ class Song < ActiveRecord::Base
     self.genre = Genre.find(id)
   end
 
+  def genre_name=(name)
+    self.genre.name = name
+
   def genre_name
     self.genre ? self.genre.name : nil
   end
